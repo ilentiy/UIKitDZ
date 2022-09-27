4cb2645616c46830be4ec758302214d6b8fb9196
ilentiy <i.brykov9@bk.ru>
2022-09-27 09:24:55 +0300

    Урок 7. Повторил видео

--- UIKitDZMain/ViewController.swift
+++ UIKitDZMain/ViewController.swift
@@ -9,8 +9,33 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    var segmentControll = UISegmentedControl()
+    var imageView = UIImageView()
+    var menu = ["one", "two", "three"]
+    var images = [UIImage(named: "обувь1.jpeg"),
+                  UIImage(named: "обувь2.jpeg"),
+                  UIImage(named: "обувь3.jpeg")]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.imageView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        self.imageView.center = self.view.center
+        self.imageView.image = self.images[0]
+        self.view.addSubview(self.imageView)
+        // create segment
+        self.segmentControll = UISegmentedControl(items: self.menu)
+        self.segmentControll.frame = CGRect(x: 100, y: 600, width: 200, height: 30)
+        self.view.addSubview(self.segmentControll)
+        
+        self.segmentControll.addTarget(self, action: #selector(selegtedValue), for: .valueChanged)
+    }
+    
+    @objc func selegtedValue(target: UISegmentedControl) {
+        if target == self.segmentControll {
+            let segmentIndex = target.selectedSegmentIndex
+            self.imageView.image = self.images[segmentIndex]
+            
+            let printTerget = target.titleForSegment(at: segmentIndex)
+            print(printTerget ?? "")
+        }
     }
 }
