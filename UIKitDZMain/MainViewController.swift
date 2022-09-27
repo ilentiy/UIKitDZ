1560d43fdf1e21c06ab125ae100c3a7d69993c77
ilentiy <i.brykov9@bk.ru>
2022-09-28 01:21:29 +0300

    Урок 8. выполнил задание

---
+++ UIKitDZMain/MainViewController.swift
@@ -0,0 +1,76 @@
+//
+//  MainViewController.swift
+//  UIKitDZMain
+//
+//  Created by Илья on 27.09.2022.
+//
+
+import UIKit
+
+/// MainViewController:  Главный контроллер
+/// - для закрепления UIActivityViewController
+final class MainViewController: UIViewController {
+    
+    private var activityViewController: UIActivityViewController?
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Methods
+    
+    @IBAction func shareButtonAction(_ sender: Any) {
+        
+        if textField.text?.count == 0 {
+            activityViewController = UIActivityViewController(activityItems: [imageView.image ?? UIImageView()],
+                                                              applicationActivities: nil)
+        } else {
+            activityViewController = UIActivityViewController(activityItems: [
+                textField.text ?? "",
+                imageView.image ?? UIImageView()
+            ],
+                                                              applicationActivities: nil)
+        }
+        guard let activityViewController = activityViewController else {
+            return
+        }
+        present(activityViewController, animated: true)
+    }
+}
+
+extension MainViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 3
+    }
+}
+
+extension MainViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "\(row+1)"
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if row == 2 {
+            if textField.text?.count == 0 {
+                activityViewController = UIActivityViewController(activityItems: [imageView.image ?? UIImageView()],
+                                                                  applicationActivities: nil)
+            } else {
+                activityViewController = UIActivityViewController(activityItems: [
+                    textField.text ?? "",
+                    imageView.image ?? UIImageView()
+                ],
+                                                                  applicationActivities: nil)
+            }
+            guard let activityViewController = activityViewController else {
+                return
+            }
+            present(activityViewController, animated: true)
+        }
+    }
+}
