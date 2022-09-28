c7989cf907a42e37b3ba91cee13d62195f3556ce
ilentiy <i.brykov9@bk.ru>
2022-09-28 23:11:01 +0300

    Исправил замечания 2

--- UIKitDZMain/ViewController.swift
+++ UIKitDZMain/ViewController.swift
@@ -8,43 +8,48 @@
 import UIKit
 
 ///  Главный контроллер, который отображает преобразованное слово
-final class ViewController: UIViewController {
+final class MainViewController: UIViewController {
     
     var helloLabel: UILabel = {
         let label = UILabel()
         label.frame = CGRect(x: 107, y: 400, width: 200, height: 50)
         return label
     }()
+    
     lazy var convertButton: UIButton = {
         let button = UIButton()
         button.frame = CGRect(x: 107, y: 300, width: 200, height: 50)
         button.setTitle("Преобразовать", for: .normal)
         button.backgroundColor = .blue
-        button.addTarget(self, action: #selector(start), for: .touchUpInside)
+        button.addTarget(self, action: #selector(startAction), for: .touchUpInside)
         return button
     }()
+    
     lazy var alertController: UIAlertController = {
         let alert = UIAlertController(title: "Вечер в хату", message: "Введите текст", preferredStyle: .alert)
-        let result = UIAlertAction(title: "ОК", style: .default) { _ in
+        let resultAction = UIAlertAction(title: "ОК", style: .default) { _ in
             guard let text = alert.textFields?.first?.text else { return }
             var checkText = CheckText()
             var resultText = checkText.check(text: text)
             self.helloLabel.text = resultText
         }
+        
         alert.addTextField()
-        alert.addAction(result)
+        alert.addAction(resultAction)
         return alert
     }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         addSubView()
     }
-    @objc func start() {
+    
+    @objc func startAction() {
         present(alertController, animated: true)
     }
+    
     func addSubView() {
         view.addSubview(convertButton)
         view.addSubview(helloLabel)
-        
     }
 }
