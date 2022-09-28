9497a7de882b8885a34b38d5d6b1f1ecac164ab7
ilentiy <i.brykov9@bk.ru>
2022-09-28 23:18:20 +0300

    исправил замечания 2

--- UIKitDZMain/Model/GuessNumberGame.swift
+++ UIKitDZMain/Model/GuessNumberGame.swift
@@ -11,13 +11,16 @@ import Foundation
 class GuessNumberGame {
     var secret: Int
     var answer: Int
+    
     init() {
         secret = 0
         answer = 0
     }
+    
     func generate() {
         secret = Int.random(in: 1...5)
     }
+    
     func isRight(answer: Int) -> Bool {
         secret == answer
     }
--- UIKitDZMain/ViewController/ViewController.swift
+++ UIKitDZMain/ViewController/ViewController.swift
@@ -9,8 +9,7 @@ import UIKit
 
 /// Стартовый Контроллер предлагающий, сложить два числа или сыграть в "Угадай число"
 final class GameViewController: UIViewController {
-    
-    var guessGame = GuessNumberGame()
+        
     lazy var additionButton: UIButton = {
         let button = UIButton()
         button.frame = CGRect(x: 107, y: 350, width: 200, height: 50)
@@ -49,25 +48,31 @@ final class GameViewController: UIViewController {
     var enterNameAlert: UIAlertController = {
         let alertController =  UIAlertController(title: "Приветстую", message: "Введите имя", preferredStyle: .alert)
         alertController.addTextField()
-        
         return alertController
     }()
+    
+    var guessGame = GuessNumberGame()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         addSubview()
     }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         enterName()
     }
+    
     @objc func buttonAdditionAction(sender: UIButton!) {
         showAdditionAlert()
     }
+    
     @objc func guessNumberAction(sender: UIButton!) {
         guessNumberAlert()
     }
 }
+
+/// Добавил алерт контроллеры
 extension GameViewController {
     func showAdditionAlert() {
         let alert = UIAlertController(title: "Могу сложить два числа", message: "введи числа", preferredStyle: .alert)
@@ -77,11 +82,9 @@ extension GameViewController {
         let resultAction = UIAlertAction(title: "Сложить", style: .default) { _ in
             guard let text = alert.textFields?.first?.text, let numberOne = Int(text) else { return }
             guard let text = alert.textFields?[1].text, let numberTwo = Int(text) else { return }
-            
             self.resultLabel.textColor = .black
             self.resultLabel.text = "\(numberOne) + \(numberTwo) = \(numberOne + numberTwo)"
             self.resultLabel.frame = CGRect(x: 107, y: 400, width: 200, height: 50)
-            
         }
         
         let cancelAction = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
@@ -89,6 +92,7 @@ extension GameViewController {
         alert.addAction(cancelAction)
         present(alert, animated: true)
     }
+    
     func guessNumberAlert() {
         let alert = UIAlertController(title: "Угадай число",
                                       message: "Я задумал число от 1 до 5, отгадаешь?",
@@ -96,6 +100,7 @@ extension GameViewController {
         alert.addTextField { textField in
             textField.delegate = self
         }
+        
         let resultAction = UIAlertAction(title: "Попробовать", style: .default) { _ in
             self.resultLabel.frame = CGRect(x: 107, y: 500, width: 200, height: 50)
             guard self.guessGame.isRight(answer: self.guessGame.answer) else {
@@ -103,18 +108,19 @@ extension GameViewController {
                 self.resultLabel.text = "Не верно я загадал \(self.guessGame.secret)"
                 return
             }
+            
             self.resultLabel.textColor = .green
             self.resultLabel.text = "Верно я загадал \(self.guessGame.secret)"
         }
-        let cancelAction = UIAlertAction(title: "Не сейчас", style: .cancel, handler: nil)
         
+        let cancelAction = UIAlertAction(title: "Не сейчас", style: .cancel, handler: nil)
         alert.addAction(resultAction)
         alert.addAction(cancelAction)
-        
         present(alert, animated: true) {
             self.guessGame.generate()
         }
     }
+    
     func addSubview() {
         self.view.addSubview(myNameLabel)
         self.view.addSubview(resultLabel)
@@ -122,6 +128,8 @@ extension GameViewController {
         self.view.addSubview(guessButton)
     }
 }
+
+/// UITextFieldDelegate
 extension GameViewController: UITextFieldDelegate {
     func textFieldDidEndEditing(_ textField: UITextField) {
         guard let text = textField.text, let number = Int(text) else { return }
@@ -133,6 +141,7 @@ extension GameViewController: UITextFieldDelegate {
             guard let myName =  self.enterNameAlert.textFields?.first?.text else { return }
             self.myNameLabel.text = myName
         }
+        
         enterNameAlert.addAction(resultAction)
         present(enterNameAlert, animated: true)
     }
